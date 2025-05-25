8e9e2682c5a3ac5de9c99f18246c1ce655fc0a38
Letícia Farias <lf182000@gmail.com>
2025-05-25 09:24:34 -0300

    edit thank you message

--- js/main.js
+++ js/main.js
@@ -108,7 +108,7 @@ document.addEventListener("DOMContentLoaded", () => {
             // Hide form and show thank you message
             rsvpForm.style.display = "none"
             thankYouMessage.innerHTML = `
-            <h3>Obrigado por confirmar sua presença, ${formDataObj.nome}!</h3>
+            <h3>Obrigado por confirmar sua presença, <p>${formDataObj.nome}!</h3></p>
             <p><strong>Email:</strong> ${formDataObj.email}</p>
             <p><strong>Telefone:</strong> ${formDataObj.telefone}</p>
             <p><strong>Acompanhantes:</strong> ${formDataObj.acompanhantes}</p>
